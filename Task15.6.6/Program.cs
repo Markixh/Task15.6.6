1da5a485c0c09991ad430044e980ff151748bb11
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-12-25 11:00:41 +0300

    Добавьте файлы проекта.

---
+++ Task15.6.6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task15_6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return (from classroom in classes
+                    from stud in classroom.Students
+                    select stud).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
